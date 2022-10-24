13f8a5629686b5f22f511fa225f22a160ffc7cd2
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2022-10-24 16:07:55 +0700

    Add project files.

---
+++ SearchAlgorithm/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithm
+{
+    class Program
+    {
+        int[] arr = new int[26];
+        int n;
+        int i;
+        static void Main(string[] args)
+        {
+        }
+    }
+}
