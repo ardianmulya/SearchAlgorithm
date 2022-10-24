de75b3f23e128112c3a5669452e9785a88e11988
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2022-10-24 17:11:58 +0700

    membuat tampilan menu

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -100,6 +100,19 @@ namespace SearchAlgorithm
         {
             Program myList = new Program();
             int pilihanmenu;
+
+            do
+            {
+                Console.WriteLine("Menu Option");
+                Console.WriteLine("==================");
+                Console.WriteLine("1.Linear Search");
+                Console.WriteLine("2.Binary Search");
+                Console.WriteLine("3.Exit");
+                Console.Write("Enter your choice (1,2,3) : ");
+                pilihanmenu = Convert.ToInt32(Console.ReadLine());
+
+                
+            }
         }
     }
 }
