9b5cf203292511d10435623f279cc30fdb4b5f6f
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2022-10-24 16:16:51 +0700

    membuat perulangan while

--- SearchAlgorithm/Program.cs
+++ SearchAlgorithm/Program.cs
@@ -8,13 +8,23 @@ namespace SearchAlgorithm
 {
     class Program
     {
-        int[] arr = new int[26];
+        int[] arr = new int[20];
         int n;
         int i;
         
         public void input()
         {
-            
+            while (true)
+            {
+                Console.Write("Enter the number of elements in the array : ");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine("\nArray should have minimun 1 and maximum 20 elements.\n");
+            }
+
         }
         static void Main(string[] args)
         {
